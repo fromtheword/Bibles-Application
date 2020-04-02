ee40869f75e5e2a7fff16b0487036b993dda0cee
Hansie <hansie.vanstraaten@gmail.com>
2020-04-02 13:15:59 +0200

    New Bibles

--- SourceCode/Bibles.Common/Formatters.cs
+++ SourceCode/Bibles.Common/Formatters.cs
@@ -43,7 +43,8 @@ namespace Bibles.Common
 
         public static int GetBibleFromKey(string bibleKey)
         {
-            if (bibleKey.IsNullEmptyOrWhiteSpace())
+            if (bibleKey.IsNullEmptyOrWhiteSpace()
+                || !Formatters.IsBiblesKey(bibleKey))
             {
                 return -1;
             }
